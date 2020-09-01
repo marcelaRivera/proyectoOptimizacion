fa28555c2e0cca0db19f77e99908db82c74f186c
4uaso <cristian.espinoza.s@usach.cl>
2020-09-01 10:03:11 -0400

    ADD: Agregando estructura inicial

--- src/main.py
+++ src/main.py
@@ -6,35 +6,51 @@ from goloso import goloso, minCost
 
 def main():
 
-	jobs = [] # Lista que almacenara a las tareas 
-	jobsCalificate = [] # Trabajos que puede hacer cada trabajador
-
-	# Realizando lectura de archivo de entrada
-	jobs, jobsCalificate =  readFile() 
-
-	O = [] #vector con las tareas superpuestas
-	P = [] #empleados calificados para hacer la tarea i
+	opcion = 0
+	jobs = None # Lista que almacenara a las tareas
+	jobsCalificate = None # Trabajos que puede hacer cada trabajador
+	O = None #vector con las tareas superpuestas
+	P = None #empleados calificados para hacer la tarea i
 	R = [] #trabajos asignados al trabajador w
-	S = [] #horas de inicio de cada trabajo 
-	
-	S = getHourInit(jobs)
-	O = constructiveO(jobs)
-	#print(O)
-	P = getEmployersCalificatesInJobs(jobsCalificate, len(jobs))
-	entrada = constructiveHeuristic(S, O, P, R, len(jobsCalificate))
-	print(entrada)
-	#print("Largo maximo" + str(len(entrada)))
-	#print("NUmero maximo es: " + str(max(entrada)))
-	#print("NUmero minimo es: " + str(min(entrada)))
-	#input("al fin ")
-	#salida = generatingNeighborhood(entrada, jobsCalificate,len(jobs),O) # len(jobs) solo para generar la solucion
-	#print(salida)
-	#input("stop")
-	# simuleted
+	S = None #horas de inicio de cada trabajo 
+	entrada = None
+	listOfCost = None
 
-	input("simuleted")
-	salidaSimuleted = simulatedAnneling2(100,1,100,0.99,entrada,len(jobsCalificate),jobsCalificate,O,len(jobs))
-	print(salidaSimuleted)
-	#input("goloso")
-	#goloso(S, O, P, R, len(jobsCalificate), jobsCalificate)
+	while opcion != 4:
+
+		print("1. Ingresar archivo")
+		print("2. Realizar análisis con método Goloso")
+		print("3. Realizar análisis con metaheuristica SA")
+		print("4. Salir")
+
+		opcion = input("Ingrese la opción: ")
+		print(opcion)
+		if opcion == "1":
+			jobs, jobsCalificate =  readFile()
+			S = getHourInit(jobs)
+			O = constructiveO(jobs)
+			P = getEmployersCalificatesInJobs(jobsCalificate, len(jobs))
+			entrada = constructiveHeuristic(S, O, P, R, len(jobsCalificate))
+			print("Opcion 1")
+
+		elif opcion == "2" and jobs is not None or jobsCalificate is not None:
+			goloso(S, O, P, R, len(jobsCalificate), jobsCalificate)
+			print("Opcion 2")
+
+		elif opcion == "3" and jobs is not None or jobsCalificate is not None:
+			salidaSimuleted = simulatedAnneling2(100,1,100,0.99,entrada,len(jobsCalificate),jobsCalificate,O,len(jobs))
+			print("Opcion 3")
+		
+		elif opcion == "4":
+			print("Opcion 4")
+			opcion = 4
+
+		elif jobs is None or jobsCalificate is None:
+			print("Primero debe cargar un archivo ")
+
+		else:
+			print("Ingrese una opcion valida")
+
+		print("\n\n")
+
 main()
